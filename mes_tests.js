8141a4d3fbacd1d1cd17e5ccc8ed1c83e7f311b8
Hiba benel harkati <87836080+HibaBenelHarkati@users.noreply.github.com>
2024-10-07 00:08:18 +0100

    Add files via upload

---
+++ mes_tests.js
@@ -0,0 +1,28 @@
+function nombre(caractere){ //cette function fait la somme des unit
+    let S=0;
+    for(let i=0;i<caractere.length;i++){
+        S+=caractere.charCodeAt(i); //le unicode assicie a chaque index de la chaine de caractere 
+    }
+    return S
+}
+/* a =nombre("Hiba"); "2344"=hiba
+console.log(a);*/
+function somme(nombre){  // la somme des chiffres parseInt
+    let s=0;
+    ch=String(nombre);
+    for(let i=0;i<ch.length;i++){
+        ch1=ch.charAt(i);  //The charAt() method returns the character at the specified index in a string.
+        s+=parseInt(ch1);
+    }
+    return s
+}
+
+console.log(somme(23));
+
+function chiffreAssocie(nombre){ //cette fonction permet de sommer les chiffres jusqu a un seul chiffre 
+    serie=somme(nombre);
+    while(serie>9){
+        serie=somme(serie);
+    }
+    return serie;
+}
